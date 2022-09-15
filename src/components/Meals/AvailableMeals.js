7014dfedbcf29b63cfa8f19c5fdbfe1cdd30cc35
DESKTOP-749QHJG\neilp <neilpoblete1@gmail.com>
2022-09-15 19:49:52 +0800

    added AvailableMeals.js & replaced data meals to  firebase

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  useEffect(async () => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://foodapp-project-14244-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json");
+        const responseData = await response.json();
+
+        const loadedMeals = [];
+        
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        }
+    };
+  }, []);
+
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
