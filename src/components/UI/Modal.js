b57ea8d071e9a2c7f5a49c1e0de17ae81d5ce483
DESKTOP-749QHJG\neilp <neilpoblete1@gmail.com>
2022-09-20 12:29:31 +0800

    added Checkout.js & validating values & feedbacks/errors

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
 
@@ -16,7 +16,6 @@ const ModalOverlay = (props) => {
 };
 
 const portalElement = document.getElementById('overlays');
-
 const Modal = (props) => {
   return (
     <Fragment>
